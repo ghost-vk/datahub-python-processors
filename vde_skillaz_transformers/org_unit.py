2b2d1eeb3ecb3504631103203af68baa22d4b1b5
Vlad Kuznetsov <ghost.vkv@gmail.com>
2024-06-28 13:11:58 +0300

    feat: add org unit fields

--- vde_skillaz_transformers/org_unit.py
+++ vde_skillaz_transformers/org_unit.py
@@ -1,23 +1,16 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class OrgUnitVde(BaseModel):
     orgunitid: str 
     name: str
     headid: Optional[str] = None
-    heademail: Optional[str]
+    heademail: Optional[EmailStr] = None
     address: Optional[str] = None
     superiorname: Optional[str] = None
     superiorid: Optional[str] 
         
-    model_config = { 'str_strip_whitespace': True }
-
-
-class OrgUnitSkillaz(BaseModel):
-    Id: str
-    Name: str
-
     model_config = { 'str_strip_whitespace': True }
 
 def validate_vde_org_unit(record):
@@ -26,6 +19,15 @@ def validate_vde_org_unit(record):
 
 def transform_org_unit_vde_to_skillaz(record):
     vde_record = validate_vde_org_unit(record)
-    skillaz_record = OrgUnitSkillaz(Id = vde_record.orgunitid, Name =
-                                    vde_record.name) 
+    skillaz_record = {
+        'Id': vde_record.orgunitid,
+        'ExternalId': vde_record.orgunitid,
+        'Name': vde_record.name,
+        'ParentId': vde_record.superiorid,
+        'Address': vde_record.address,
+        'IsArchived': False,
+        'Data': {
+            'ExtraData.Manager': vde_record.heademail
+        }
+    }
     return skillaz_record
--- vde_skillaz_transformers/tests/test_org_unit_functions.py
+++ vde_skillaz_transformers/tests/test_org_unit_functions.py
@@ -5,12 +5,17 @@ def test_transform_org_unit_vde_to_skillaz():
         'orgunitid': '70054048-87c5-4c07-94eb-bbff34e8eb5b',
         'name': ' Руководство ',
         'headid': '64a2cfa0-5651-448c-9e05-2af1afee9deb',
-        'heademail': 'head@digital.gov.ru',
+        'heademail': '  head@digital.gov.ru  ',
         'address': 'Москва',
         'superiorname': 'Сверхруководство',
         'superiorid': '113587e5-e3ce-4302-948b-309df04951e2',
     }
     transformed = ou.transform_org_unit_vde_to_skillaz(record)
-    assert transformed.Id == record['orgunitid'].strip()
-    assert transformed.Name == record['name'].strip()
+    assert transformed['Id'] == record['orgunitid'].strip()
+    assert transformed['ExternalId'] == record['orgunitid'].strip()
+    assert transformed['Name'] == record['name'].strip()
+    assert transformed['ParentId'] == record['superiorid'].strip()
+    assert transformed['Address'] == record['address'].strip()
+    assert transformed['IsArchived'] == False
+    assert transformed['Data']['ExtraData.Manager'] == record['heademail'].strip()
 
