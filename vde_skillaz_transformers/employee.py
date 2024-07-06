20d02a38d2681db04f69ee48f10139f44685cfa9
Vlad Kuznetsov <ghost.vkv@gmail.com>
2024-07-06 12:33:58 +0300

    feat: employee transformer

--- vde_skillaz_transformers/employee.py
+++ vde_skillaz_transformers/employee.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List, Optional, TypeAlias, TypedDict, Any
+from typing import Optional, TypeAlias, TypedDict, Any
 from enum import Enum
 
 from pydantic import BaseModel, EmailStr, ValidationError, field_validator, ConfigDict
@@ -32,6 +32,9 @@ class EmployeeRolesUUIDEnum(str, Enum):
     mentor = '54201b65-eb88-48a4-81ff-6d3091c7f7b0'
     external_partner = 'c53ab918-bbb8-4d29-b51e-50d4991f0f8c'
 
+def employee_role_uuid_to_name(uuid: EmployeeRolesUUIDEnum) -> EmployeeRolesEnum:
+    return EmployeeRolesEnum[uuid.name]
+
 class EmployeeGenderEnum(str, Enum):
     m = 'Мужской'
     f = 'Женский'
@@ -50,9 +53,10 @@ def validate_phone_number(
     raise TransformException('Phone Number Invalid.')
 
 class PEmployeeVde(BaseModel):
+    """Представление сотрудника в исходных данных"""
     workplaceaddress: Optional[str] = None
     state: Optional[EmployeeStateEnum] = None
-    gender: Optional[EmployeeGenderEnum] = None
+    gender: EmployeeGenderEnum
     workemail: EmailStr
     phone: Optional[str] = None
     workphone: Optional[str] = None
@@ -60,7 +64,7 @@ class PEmployeeVde(BaseModel):
     name: str
     patronymic: Optional[str] = None
     snils: Optional[str] = None
-    personnelnumber: Optional[str] = None
+    personnelnumber: str
     jobid: Optional[str] = None
     jobname: Optional[str] = None
     workfunc: Optional[str] = None
@@ -87,10 +91,58 @@ class PEmployeeVde(BaseModel):
             return v
         return validate_phone_number(v)
 
+class PEmployeeVdeMeta(BaseModel):
+    """Представление сотрудника с метаданными из БД"""
+    # новые поля
+    id: str
+    functionalmanager: Optional[str]
+    functionalmanagereml: Optional[EmailStr]
+    # uuid вместо названий
+    state: Optional[str] = None
+    isarchived: Optional[bool] = False
+    roles: Optional[str]
+
+    # дальше поля из PEmployeeVde
+    workplaceaddress: Optional[str] = None
+    gender: EmployeeGenderEnum
+    workemail: EmailStr
+    phone: Optional[str] = None
+    workphone: Optional[str] = None
+    surname: str 
+    name: str
+    patronymic: Optional[str] = None
+    snils: Optional[str] = None
+    personnelnumber: str
+    jobid: Optional[str] = None
+    jobname: Optional[str] = None
+    workfunc: Optional[str] = None
+    orgunitid: Optional[str] = None
+    orgunitname: Optional[str] = None
+    datejobassignment: Optional[datetime] = None
+    dateorgentry: Optional[datetime] = None
+    dateofbirth: Optional[datetime] = None
+    flowuuid: Optional[str] = None
+
+    model_config = ConfigDict(
+        coerce_numbers_to_str=True
+    )
+
+    @field_validator('phone')
+    def phone_validation(cls, v):
+        if v is None:
+            return v
+        return validate_phone_number(v)
+
+    @field_validator('workphone')
+    def workphone_validation(cls, v):
+        if v is None:
+            return v
+        return validate_phone_number(v)
+    
 class EmployeeVdeInsert(TypedDict):
     workplaceaddress: Optional[str]
     state: Optional[EmployeeStateUUIDEnum]
-    gender: Optional[EmployeeGenderEnum]
+    gender: EmployeeGenderEnum
     workemail: str
     phone: Optional[ValidatedPhoneNumber] 
     workphone: Optional[ValidatedPhoneNumber] 
@@ -98,7 +150,7 @@ class EmployeeVdeInsert(TypedDict):
     name: str 
     patronymic: Optional[str] 
     snils: Optional[str] 
-    personnelnumber: Optional[str] 
+    personnelnumber: str
     jobid: Optional[str] 
     jobname: Optional[str] 
     workfunc: Optional[str] 
@@ -116,7 +168,8 @@ def transform_employee_vde_insert(record: dict[str, Any]) -> EmployeeVdeInsert:
         employee = PEmployeeVde(**strip_record)
         insert_record: EmployeeVdeInsert = {
             'workplaceaddress': employee.workplaceaddress,
-            'state': EmployeeStateUUIDEnum[employee.state.name] if employee.state else None,
+            'state': EmployeeStateUUIDEnum[employee.state.name] 
+                if employee.state else None,
             'gender': employee.gender,
             'workemail': employee.workemail,
             'phone':  employee.phone,
@@ -146,11 +199,128 @@ class EmployeeSkillazInternationalPhoneNumber(TypedDict):
     AreaCode: str
     CountryCode: str
 
+EmployeeSkillazData = TypedDict('EmployeeSkillazData', {
+    # 'м' | 'ж' | 'f' | 'm'
+    'ExtraData.gender': str,
+    'ExtraData.WorkPhoneNumber': Optional[ValidatedPhoneNumber],
+    'ExtraData.SNILS': Optional[str],
+    'ExtraData.PositionId': Optional[str],
+    'ExtraData.FunctionalManager': Optional[str],
+    'ExtraData.city': Optional[str], 
+    'ExtraData.WorkFunction': Optional[str],
+    'ExtraData.State': Optional[str],
+    'ExtraData.department_external_ids': Optional[list[str]],
+    # datetime in iso8601 format
+    'ExtraData.date_of_hire_job_position': Optional[str],
+    'ExtraData.date_of_hire': Optional[str],
+    'ExtraData.date_of_birth': Optional[str],
+})
+
 class EmployeeSkillaz(TypedDict):
     UserName: str
     Email: str 
     LastName: str
     FirstName: str
     MiddleName: Optional[str]
-    RoleIds: Optional[List[EmployeeRolesEnum]]
+    RoleIds: Optional[list[str]]
     InternationalPhoneNumber: Optional[EmployeeSkillazInternationalPhoneNumber]
+    Position: Optional[str]
+    EmployeeId: str
+    IsArchived: Optional[bool]
+    Data: EmployeeSkillazData
+
+def _split_role_uuids(input: str) -> list[EmployeeRolesEnum]:
+    try:
+        if input == '' or input is None:
+            return []
+        return list(map(
+            lambda uuid: EmployeeRolesEnum[EmployeeRolesUUIDEnum(uuid).name],
+            input.split(',')))
+    except ValueError:
+        raise TransformException('bad role')
+
+def _map_role_values(roles: list[EmployeeRolesEnum]) -> list[str]:
+    if len(roles) == 0:
+        return []
+    return list(map(lambda r: r.value, roles))
+
+def _transform_role_uuids_to_names(input: str | None) -> list[str] | None:
+    if input is None:
+        return None
+    roles = _split_role_uuids(input)
+    return _map_role_values(roles)
+
+def _split_state_uuids(input: str) -> list[EmployeeStateEnum]:
+    try:
+        if input == '':
+            return []
+        return list(map(
+            lambda uuid: EmployeeStateEnum[EmployeeStateUUIDEnum(uuid).name],
+            input.split(',')))
+    except ValueError:
+        raise TransformException('bad state')
+
+def _map_state_values(states: list[EmployeeStateEnum]) -> list[str]:
+    if len(states) == 0:
+        return []
+    return list(map(lambda s: s.value, states))
+
+def _transform_state_uuids_to_names(input: str | None) -> str | None:
+    if input is None:
+        return None
+    states = _split_state_uuids(input)
+    return _map_state_values(states)[0]
+
+def _datetime_to_iso_or_none(_datetime: datetime | None=None) -> str | None:
+    return _datetime.isoformat() if _datetime else None
+
+def _format_phone_ru_international(phone: ValidatedPhoneNumber | None) -> EmployeeSkillazInternationalPhoneNumber | None:
+        if phone is None:
+            return None
+        return {
+            'PhoneNumber': phone,
+            'AreaCode': '+7',
+            'CountryCode': 'RU',
+        }
+        
+def _extract_gender_first_char(gender: EmployeeGenderEnum) -> str:
+    return gender.value[0].lower()
+
+def transform_employee_skillaz(record: dict[str, Any]) -> EmployeeSkillaz:
+    try:
+        record = strip_dict_strings(record)
+        validated = PEmployeeVdeMeta(**record)
+        employee_skillaz: EmployeeSkillaz = {
+            'UserName': validated.workemail, 
+            'Email': validated.workemail, 
+            'LastName': validated.surname,
+            'FirstName': validated.name,
+            'MiddleName': validated.patronymic,
+            'RoleIds': _transform_role_uuids_to_names(validated.roles),
+            'InternationalPhoneNumber': _format_phone_ru_international(
+                validated.phone),
+            'IsArchived': validated.isarchived,
+            'Position': validated.jobname,
+            'EmployeeId': validated.personnelnumber,
+            'Data': {
+                'ExtraData.gender': _extract_gender_first_char(validated.gender),
+                'ExtraData.WorkPhoneNumber': validated.workphone,
+                'ExtraData.SNILS': validated.snils,
+                'ExtraData.PositionId': validated.jobid,
+                'ExtraData.FunctionalManager': validated.functionalmanagereml,
+                'ExtraData.city': validated.workplaceaddress, 
+                'ExtraData.WorkFunction': validated.workfunc,
+                'ExtraData.State': _transform_state_uuids_to_names(validated.state),
+                'ExtraData.department_external_ids': [validated.orgunitid] 
+                    if validated.orgunitid else None,
+                'ExtraData.date_of_hire_job_position':
+                    _datetime_to_iso_or_none(validated.datejobassignment),
+                'ExtraData.date_of_hire': _datetime_to_iso_or_none(validated.dateorgentry),
+                'ExtraData.date_of_birth': _datetime_to_iso_or_none(validated.dateofbirth),
+            }
+        }
+        return employee_skillaz
+    except ValidationError as verr:
+        print(verr)
+        raise TransformException
+    
--- vde_skillaz_transformers/tests/test_employee.py
+++ vde_skillaz_transformers/tests/test_employee.py
@@ -1,6 +1,10 @@
 import datetime 
+import pprint
+
+import pytest
 
 import employee as e
+from exceptions import TransformException
 
 def test_transform_employee_vde_insert_valid_case():
     record = {
@@ -14,7 +18,7 @@ def test_transform_employee_vde_insert_valid_case():
         'name': 'Петр',
         'patronymic': None,
         'snils': 1231231234,
-        'personnelnumber': None,
+        'personnelnumber': '123-123-123',
         'jobid': ' 070f2899-5933-4d56-bb22-02644cf47320 ',
         'jobname': ' Технический специалист 1С ',
         'workfunc': ' Администрирование \n1C системы ',
@@ -37,7 +41,7 @@ def test_transform_employee_vde_insert_valid_case():
         'name': 'Петр',
         'patronymic': None,
         'snils': '1231231234',
-        'personnelnumber': None,
+        'personnelnumber': '123-123-123',
         'jobid': '070f2899-5933-4d56-bb22-02644cf47320',
         'jobname': 'Технический специалист 1С',
         'workfunc': 'Администрирование \n1C системы',
@@ -49,3 +53,93 @@ def test_transform_employee_vde_insert_valid_case():
         'flowuuid': '34f5aced-d03d-4fa3-8105-ec8fcb3ff8c1',
         'record_name': 'p.ivanov@digital.gov.ru',
     }
+
+def test_transform_employee_skillaz_valid_case():
+    record = {
+        'id': '8af42d5b-2a06-4fbd-8804-788c73e6790a',
+        'functionalmanager': '52f03312-8676-4a5d-901a-3c6e06c192c3',
+        'functionalmanagereml': 'f.manager@digital.gov.ru',
+        'workplaceaddress': ' Москва  ',
+        'roles': ' a1f00ef8-b6a6-4766-91b9-4ec55cb4ab62',
+        'state': ' 9c306aec-b4dc-4e26-9724-be024fe4d194',
+        'gender': '  Мужской  ',
+        'workemail': 'p.ivanov@digital.gov.ru',
+        'phone':  '  79887776655  ',
+        'workphone': '  79887776633  ',
+        'surname': 'Иванов',
+        'name': 'Петр',
+        'patronymic': None,
+        'snils': 1231231234,
+        'personnelnumber': '123-123-123',
+        'jobid': ' 070f2899-5933-4d56-bb22-02644cf47320 ',
+        'jobname': ' Технический специалист 1С ',
+        'workfunc': ' Администрирование \n1C системы ',
+        'orgunitid': ' ff7cbb2b-3951-4329-a736-c4800ae62ceb ',
+        'orgunitname': 'Управление 1С системами ',
+        'datejobassignment': 1404299569,
+        'dateorgentry': None,
+        'dateofbirth': None,
+        'flowuuid': '34f5aced-d03d-4fa3-8105-ec8fcb3ff8c1',
+    }
+    rec = e.transform_employee_skillaz(record)
+    assert rec == {
+        'UserName': 'p.ivanov@digital.gov.ru',
+        'Email': 'p.ivanov@digital.gov.ru',
+        'LastName': 'Иванов',
+        'FirstName': 'Петр',
+        'MiddleName': None,
+        'RoleIds': [e.EmployeeRolesEnum.user],
+        'InternationalPhoneNumber': {
+            'PhoneNumber': '+7 988 777-66-55',
+            'AreaCode': '+7',
+            'CountryCode': 'RU',
+        },
+        'IsArchived': False,
+        'Position': 'Технический специалист 1С',
+        'EmployeeId': '123-123-123',
+        'Data': {
+            'ExtraData.gender': 'м',
+            'ExtraData.WorkPhoneNumber': '+7 988 777-66-33',
+            'ExtraData.SNILS': '1231231234',
+            'ExtraData.PositionId': '070f2899-5933-4d56-bb22-02644cf47320',
+            'ExtraData.FunctionalManager': 'f.manager@digital.gov.ru',
+            'ExtraData.city': 'Москва',
+            'ExtraData.WorkFunction': 'Администрирование \n1C системы',
+            'ExtraData.State': e.EmployeeStateEnum.sick.value,
+            'ExtraData.department_external_ids': ['ff7cbb2b-3951-4329-a736-c4800ae62ceb'],
+            'ExtraData.date_of_hire_job_position': '2014-07-02T11:12:49+00:00',
+            'ExtraData.date_of_hire': None,
+            'ExtraData.date_of_birth': None},
+        }
+    # pprint.pp(rec)
+
+def test_transform_employee_skillaz_not_valid_case():
+    with pytest.raises(TransformException) as e_info:
+        bad_state_record = {
+            'id': '8af42d5b-2a06-4fbd-8804-788c73e6790a',
+            'functionalmanager': '52f03312-8676-4a5d-901a-3c6e06c192c3',
+            'functionalmanagereml': 'f.manager@digital.gov.ru',
+            'workplaceaddress': ' Москва  ',
+            'roles': ' a1f00ef8-b6a6-4766-91b9-4ec55cb4ab62',
+            'state': ' 9c306aec-b4dc-4e26-9724-be024fe4d195',
+            'gender': '  Мужской  ',
+            'workemail': 'p.ivanov@digital.gov.ru',
+            'phone':  '  79887776655  ',
+            'workphone': '  79887776633  ',
+            'surname': 'Иванов',
+            'name': 'Петр',
+            'patronymic': None,
+            'snils': 1231231234,
+            'personnelnumber': '123-123-123',
+            'jobid': ' 070f2899-5933-4d56-bb22-02644cf47320 ',
+            'jobname': ' Технический специалист 1С ',
+            'workfunc': ' Администрирование \n1C системы ',
+            'orgunitid': ' ff7cbb2b-3951-4329-a736-c4800ae62ceb ',
+            'orgunitname': 'Управление 1С системами ',
+            'datejobassignment': 1404299569,
+            'dateorgentry': None,
+            'dateofbirth': None,
+            'flowuuid': '34f5aced-d03d-4fa3-8105-ec8fcb3ff8c1',
+        }
+        e.transform_employee_skillaz(bad_state_record)
+    assert e_info.value.args[0] == 'bad state'
